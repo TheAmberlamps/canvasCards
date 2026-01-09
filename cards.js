3aa3668266b030355e5d83ca340733a19f4de09b
Paul Unsted <ed__84@hotmail.com>
2026-01-09 08:27:49 -0500

    animations for multiple cards solved, main update loop established

--- cards.js
+++ cards.js
@@ -25,71 +25,88 @@ function randRotInRads() {
   }
 }
 
-let cardArr = []
+function randOffset(val) {
+  if (Math.random() > 0.5) {
+    return Math.floor(Math.random() * val);
+  } else {
+    return -Math.floor(Math.random() * val);
+  }
+}
+
+let cardArr = [];
 
 async function cardMaker(deck) {
   let newCard = new Image();
-  newCard.src = await drawCards(deck)
+  newCard.src = await drawCards(deck);
   newCard.addEventListener("load", () => {
-  newCard.valObj = {
-    xVal: 0,
-    yVal: canvas.height,
-    rotateVal: 0,
-  };
-  gsap.to(newCard.valObj, {
-    rotateVal: randRotInRads(),
-    xVal: canvas.width / 2,
-    yVal: canvas.height / 2,
-    ease: "power4.out",
-    duration: 1,
+    newCard.valObj = {
+      xVal: 0,
+      yVal: canvas.height,
+      rotateVal: 0,
+    };
+    gsap.to(newCard.valObj, {
+      rotateVal: randRotInRads(),
+      xVal: canvas.width / 2 + randOffset(100),
+      yVal: canvas.height / 2 + randOffset(100),
+      ease: "power4.out",
+      duration: 1,
+    });
+    cardArr.push(newCard);
   });
-  // this should be its own drawing function that clears the screen, loops through all cardArr objects and draws them based on their internal values
-  // commiting changes now before experimenting further, lots of progress made so far
-  gsap.ticker.add(function () {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.save();
-    ctx.translate(newCard.valObj.xVal, newCard.valObj.yVal);
-    ctx.rotate(newCard.valObj.rotateVal);
-    ctx.drawImage(newCard, -newCard.width / 2, -newCard.height / 2);
-    ctx.restore();
-  });
-});
 }
 
 let decks = 1;
 
-async function genDeck (decks) {
+async function genDeck(decks) {
   let deckUrl = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=${decks}`;
-  
+
   try {
     let response = await fetch(deckUrl);
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
     let data = await response.json();
-    return data.deck_id
+    return data.deck_id;
   } catch (error) {
-    console.log("Fetch failed: ", error)
-    return null
+    console.log("Fetch failed: ", error);
+    return null;
   }
 }
 
-let newDeck = await genDeck(decks)
+let newDeck = await genDeck(decks);
 
-console.log(newDeck)
+console.log(newDeck);
 
 async function drawCards(newDeck) {
   let drawCard = `https://deckofcardsapi.com/api/deck/${newDeck}/draw/?count=1`;
   try {
-    let response = await fetch(drawCard)
-  if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-  let data = await response.json();
+    let response = await fetch(drawCard);
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    let data = await response.json();
     console.log(data.cards[0]);
     return data.cards[0].image;
   } catch (error) {
-    console.log("Fetch failed: ", error)
-    return null
+    console.log("Fetch failed: ", error);
+    return null;
   }
 }
 
-for (let i=0; i < 5; i++) {
-  await cardMaker(newDeck)
+for (let i = 0; i < 5; i++) {
+  // I like this
+  let time = (i + 1) * 500;
+  setTimeout(async function () {
+    await cardMaker(newDeck);
+  }, time);
 }
+
+// update loop
+gsap.ticker.add(function () {
+  if (cardArr.length > 0) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < cardArr.length; i++) {
+      ctx.save();
+      ctx.translate(cardArr[i].valObj.xVal, cardArr[i].valObj.yVal);
+      ctx.rotate(cardArr[i].valObj.rotateVal);
+      ctx.drawImage(cardArr[i], -cardArr[i].width / 2, -cardArr[i].height / 2);
+      ctx.restore();
+    }
+  }
+});
